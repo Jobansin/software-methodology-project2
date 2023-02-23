fe39761a3d3f3035dc3c505fae9abfbaf2223a64
David <davidhariantowu@gmail.com>
2023-02-23 00:14:25 -0500

    Added comments and finished the required methods for now

--- src/project2/EnrollStudent.java
+++ src/project2/EnrollStudent.java
@@ -1,14 +1,43 @@
 package project2;
 
+/**
+ Represents an EnrollStudent object which holds a Profile object and credits completed.
+ @author David Harianto, Joban Singh
+ **/
 public class EnrollStudent {
     private Profile profile;
     private int creditsEnrolled;
 
-    @Override
-    public boolean equals(Object object) {
-
+    /**
+     This is a constructor that takes in 2 parameters for an EnrollStudent object.
+     @author David Harianto, Joban Singh
+     **/
+    public EnrollStudent(Profile p, int credits) {
+        profile = p;
+        creditsEnrolled = credits;
     }
 
+    /**
+     This method checks if the student given is the same student.
+     @author David Harianto, Joban Singh
+     **/
+    @Override
+    public boolean equals(Object object) {
+        // Compare two students
+        if(this == object) {
+            return true;
+        }
+        if(!(object instanceof Student)) {
+            return false;
+        }
+        EnrollStudent s = (EnrollStudent) object;
+        return this.profile.equals(s.profile);
+    }
+
+    /**
+     This method returns the first name, last name, date of birth, and credits completed.
+     @author David Harianto, Joban Singh
+     **/
     public String toString() {
         return profile + " credits completed: " + creditsEnrolled;
     }
